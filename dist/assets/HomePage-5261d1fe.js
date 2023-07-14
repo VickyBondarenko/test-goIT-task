94a154a248ec5d811e34c2a77b15dc9db45b711f
VickyBondarenko <quikyv@gmail.com>
2023-07-14 17:47:28 +0200

    add

---
+++ dist/assets/HomePage-5261d1fe.js
@@ -0,0 +1,19 @@
+import{j as e,s as t}from"./index-3336a8b2.js";function s(){return e.jsx(o,{children:e.jsx(n,{children:"Welcome to home page !"})})}const o=t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`,n=t.div`
+  padding: 80px;
+  border-radius: 20px;
+  flex-shrink: 0;
+  background-color: #ebd8ff;
+  color: #4b2a99;
+  font-family: Montserrat;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  text-transform: uppercase;
+  box-shadow: 1px 1px 5px 1px #4b2a99;
+`;export{s as Home,s as default};
