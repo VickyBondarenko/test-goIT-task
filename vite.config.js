5cbeb8489808e78a7f4a56aeb0dc6e432bb38e5b
VickyBondarenko <quikyv@gmail.com>
2023-07-14 18:22:02 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/test-goit-task",
+  base: "https://github.com/VickyBondarenko/test-goIT-task",
   plugins: [react()],
 });
