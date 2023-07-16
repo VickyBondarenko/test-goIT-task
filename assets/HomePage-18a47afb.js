d7f6dbedb0ed1807218983c9f9691e640541be41
VickyBondarenko <quikyv@gmail.com>
2023-07-16 15:07:19 +0200

    Updates

--- assets/HomePage-18a47afb.js
+++ assets/HomePage-18a47afb.js
@@ -1,4 +1,4 @@
-import{j as e,s as t}from"./index-6897d46f.js";function s(){return e.jsx(o,{children:e.jsx(n,{children:"Welcome to home page !"})})}const o=t.div`
+import{j as e,s as t}from"./index-b7bd5116.js";function s(){return e.jsx(o,{children:e.jsx(n,{children:"Welcome to home page !"})})}const o=t.div`
   display: flex;
   justify-content: center;
   align-items: center;
